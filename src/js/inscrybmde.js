13909e43355d78b1b5b8a588de9039b68f65f126
Martin Korndörfer <martin@korndoerfer.de>
2018-10-30 22:54:37 +0100

    - Added the ability to switch between a textarea and the full editor on the fly. - Added method 'toEditor' - Added option 'autoRender' - Added documentation and example. - Updated CodeMirror

--- src/js/inscrybmde.js
+++ src/js/inscrybmde.js
@@ -1434,14 +1434,26 @@ function InscrybMDE(options) {
 
 
     // Auto render
-    this.render();
+    var autoRender = true;
+    if (options.autorender === false) {
+        autoRender = false;
+    }
 
+    if(autoRender) {
+        this.render();
 
-    // The codemirror component is only available after rendering
-    // so, the setter for the initialValue can only run after
-    // the element has been rendered
-    if (options.initialValue && (!this.options.autosave || this.options.autosave.foundSavedValue !== true)) {
-        this.value(options.initialValue);
+        // The codemirror component is only available after rendering
+        // so, the setter for the initialValue can only run after
+        // the element has been rendered
+        if (options.initialValue && (!this.options.autosave || this.options.autosave.foundSavedValue !== true)) {
+            this.value(options.initialValue);
+        }
+    }
+    else {
+        if (options.initialValue) {
+            // Set value of standard text area
+            this.element.value = options.initialValue;
+        }
     }
 }
 
@@ -1496,8 +1508,8 @@ InscrybMDE.prototype.render = function (el) {
         el = this.element || document.getElementsByTagName('textarea')[0];
     }
 
-    if (this._rendered && this._rendered === el) {
-        // Already rendered.
+    if (this.codemirror != undefined) {
+        // Already rendered and active
         return;
     }
 
@@ -1575,13 +1587,22 @@ InscrybMDE.prototype.render = function (el) {
         });
     }
 
-    this.gui = {};
+    if(this.gui == undefined)
+        this.gui = {};
 
+    var cmWrapper = this.codemirror.getWrapperElement();
     if (options.toolbar !== false) {
-        this.gui.toolbar = this.createToolbar();
+        if(this.gui.toolbar == undefined)
+            this.gui.toolbar = this.createToolbar();
+
+        cmWrapper.parentNode.insertBefore(this.gui.toolbar, cmWrapper);
     }
+
     if (options.status !== false) {
-        this.gui.statusbar = this.createStatusbar();
+        if(this.gui.statusbar == undefined)
+            this.gui.statusbar = this.createStatusbar();
+
+        cmWrapper.parentNode.insertBefore(this.gui.statusbar, cmWrapper.nextSibling);
     }
     if (options.autosave != undefined && options.autosave.enabled === true) {
         this.autosave();
@@ -1822,8 +1843,6 @@ InscrybMDE.prototype.createToolbar = function (items) {
         }
     });
 
-    var cmWrapper = cm.getWrapperElement();
-    cmWrapper.parentNode.insertBefore(bar, cmWrapper);
     return bar;
 };
 
@@ -1937,8 +1956,6 @@ InscrybMDE.prototype.createStatusbar = function (status) {
 
 
     // Insert the status bar into the DOM
-    var cmWrapper = this.codemirror.getWrapperElement();
-    cmWrapper.parentNode.insertBefore(bar, cmWrapper.nextSibling);
     return bar;
 };
 
@@ -2103,11 +2120,17 @@ InscrybMDE.prototype.toTextArea = function () {
 
     cm.toTextArea();
 
+    this.codemirror = undefined;
+
     if (this.autosaveTimeoutId) {
         clearTimeout(this.autosaveTimeoutId);
         this.autosaveTimeoutId = undefined;
         this.clearAutosavedValue();
     }
+};
+
+InscrybMDE.prototype.toEditor = function () {
+    this.render();
 };
 
 module.exports = InscrybMDE;
